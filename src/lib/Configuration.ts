8882c0f05e4b247fe079b2f107da4092b07b88ed
trifledmatter <ethan@gradison.ca>
2024-10-14 11:35:22 -0300

    remove required assertion for pkg in config

--- src/lib/Configuration.ts
+++ src/lib/Configuration.ts
@@ -1,8 +1,7 @@
 import type { PermissionsString } from 'discord.js';
-import pkg from '../../package.json' assert { type: 'json' };
 
 export const token = process.env.DISCORD_TOKEN as string;
-export const version = (process.env.CLIENT_VERSION ??= pkg.version);
+export const version = (process.env.CLIENT_VERSION ??= '0');
 export const owners = process.env.CLIENT_OWNERS?.split(',').filter((item) => item.length) as string[];
 export const debug = process.env.DEBUG_MODE === 'true';
 export const defaultPermissions = ['SendMessages', 'ViewChannel'] as PermissionsString[];
